461ccc4ac182dd04a5164ea9e6675a5dd3b089a8
Devinder Khroad <dkhroad@gmail.com>
2013-10-14 18:26:26 -0700

    fix typo in nrsysmond  config file attribute
    should be log_level and log_file instead of loglevel and logfile

--- attributes/nr_sysmond.rb
+++ attributes/nr_sysmond.rb
@@ -1,4 +1,4 @@
-default[:newrelic_monitoring][:nr_sysmond][:loglevel] = "info"
-default[:newrelic_monitoring][:nr_sysmond][:logfile] = "/var/log/newrelic/nr_sysmond.log"
+default[:newrelic_monitoring][:nr_sysmond][:log_level] = "info"
+default[:newrelic_monitoring][:nr_sysmond][:log_file] = "/var/log/newrelic/nr_sysmond.log"
 default[:newrelic_monitoring][:nr_sysmond][:ssl] = true
 default[:newrelic_monitoring][:nr_sysmond][:timeout] = 30
